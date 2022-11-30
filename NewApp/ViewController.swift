ae15f4e9a995da63a492e0561de113f1aaee9971
Alexey <panda3er@gmail.com>
2022-11-30 14:17:08 +0300

    Initial Commit

---
+++ NewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewApp
+//
+//  Created by MacBook on 30.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
